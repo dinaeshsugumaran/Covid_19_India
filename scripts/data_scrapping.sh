0c534dcb663663571128724eb2c915c655b90f3f
dinaeshsugumaran <54409249+dinaeshsugumaran@users.noreply.github.com>
2020-04-18 14:49:41 -0400

    Initial commit

---
+++ scripts/data_scrapping.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jupyter nbconvert --to notebook --inplace --execute  /Users/dinaeshs/Covid_19_India/notebooks/COVID-19_India.ipynb
---
+++ scripts/graph.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jupyter nbconvert --to notebook --inplace --execute  /Users/dinaeshs/Covid_19_India/notebooks/Covid_ind_data_analysis.ipynb
